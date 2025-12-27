496b202dfbad3ca7f8350faf98547420843f12f6
saksham-pahadi <saksham-pahadi@users.noreply.github.com>
2025-12-27 16:08:37 +0530

    Basic TypeScript Setup

---
+++ Tut-01/dist/basic.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greeet(name) {
+    return `Hello, ${name}!`;
+}
+const username = "World";
+console.log(greeet(username)); // Output: Hello, World!
+//# sourceMappingURL=basic.js.map
---
+++ Tut-01/src/basic.ts
@@ -0,0 +1,5 @@
+function greeet(name: string): string {
+    return `Hello, ${name}!`
+}
+const username: string = "World";
+console.log(greeet(username)); // Output: Hello, World!
